2d6b72ee7798f330a165d6e6d9899d18ac6a0a79
Bartosz Różański <bartek.roza@gmail.com>
2025-04-24 12:26:05 +0200

    don't setup PLC server in dev-env

--- packages/dev-env/src/plc.ts
+++ packages/dev-env/src/plc.ts
@@ -1,5 +1,5 @@
 import { Client as PlcClient } from '@did-plc/lib'
-import * as plc from '@did-plc/server'
+//import * as plc from '@did-plc/server'
 import getPort from 'get-port'
 import { PlcConfig } from './types'
 
@@ -7,27 +7,27 @@ export class TestPlc {
   constructor(
     public url: string,
     public port: number,
-    public server: plc.PlcServer,
-  ) {}
+    //public server: plc.PlcServer,
+  ) { }
 
   static async create(cfg: PlcConfig): Promise<TestPlc> {
-    const db = plc.Database.mock()
+    //const db = plc.Database.mock()
     const port = cfg.port || (await getPort())
     const url = `http://localhost:${port}`
-    const server = plc.PlcServer.create({ db, port, ...cfg })
-    await server.start()
-    return new TestPlc(url, port, server)
+    //const server = plc.PlcServer.create({ db, port, ...cfg })
+    //await server.start()
+    return new TestPlc(url, port)
   }
 
-  get ctx(): plc.AppContext {
-    return this.server.ctx
-  }
+  //get ctx(): plc.AppContext {
+  //  return this.server.ctx
+  //}
 
   getClient(): PlcClient {
     return new PlcClient(this.url)
   }
 
   async close() {
-    await this.server.destroy()
+    //await this.server.destroy()
   }
 }
